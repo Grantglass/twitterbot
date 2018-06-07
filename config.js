60c66522ab6ea69939eafefc8807a025b4d643af
Grant Glass <Grant.glass@gmail.com>
2018-06-07 14:59:06 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'NkRA8zc4w7GvRHlOw8FiNUogE',
+  consumer_secret:      'O1l3aTvAhmOR4gXoMxWUe0S107HbB60UoP5gMjco5XdWOh5C2I',
+  access_token:         '1004824139780186112-sO1l8rLouoiL36S7JeGIIjgZG7vC5F',
+  access_token_secret:  'FWTKtl9R5fTwSJvxaKcO4HoaegdKWE4SG311Bu3DlNSzM'
+}
